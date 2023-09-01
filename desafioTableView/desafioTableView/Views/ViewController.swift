276b68955dca659c100d7e41b0c31c42beb50d25
Leticia Speda <leticiaspeda@gmail.com>
2023-09-01 12:48:00 -0300

    build: configuracoes para iniciar o simulador

---
+++ desafioTableView/desafioTableView/Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  desafioTableView
+//
+//  Created by Leticia Speda on 01/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+
+
+}
+
